61575a45e0efcbf3355949dcd2adbe383ee86abb
NataliaReiis <nataliareis208@gmail.com>
2023-12-14 02:14:23 -0300

    feat: page contact

--- src/components/contact/Form.jsx
+++ src/components/contact/Form.jsx
@@ -1,40 +1,73 @@
-import { useForm, ValidationError } from '@formspree/react';
+import { useForm, ValidationError } from "@formspree/react";
 
+import MailOutlineOutlinedIcon from "@mui/icons-material/MailOutlineOutlined";
+import WhatsAppIcon from "@mui/icons-material/WhatsApp";
+import LocationOnOutlinedIcon from "@mui/icons-material/LocationOnOutlined";
 
-
- export default function ContactForm() {
-
-  const [state, handleSubmit] = useForm("xnqkevvd");
+export default function Form() {
+  const [state, handleSubmit] = useForm("xnqkevvd"); /* Formulario */
   if (state.succeeded) {
-      return <p>Thanks for joining!</p>;
+    return <p>Thanks for joining!</p>;
   }
+
   return (
-      <form onSubmit={handleSubmit}>
-      <label htmlFor="email">
-        Email Address
-      </label>
-      <input
-        id="email"
-        type="email" 
-        name="email"
-      />
-      <ValidationError 
-        prefix="Email" 
-        field="email"
-        errors={state.errors}
-      />
-      <textarea
-        id="message"
-        name="message"
-      />
-      <ValidationError 
-        prefix="Message" 
-        field="message"
-        errors={state.errors}
-      />
-      <button type="submit" disabled={state.submitting}>
-        Submit
-      </button>
-    </form>
+    <div className="contact-container">
+      <div className="contact-infos-container">
+        <h1>Desvende o potencial do seu negócio: Tire suas dúvidas!</h1>
+        <div className="infos">
+          <span>
+            <MailOutlineOutlinedIcon />
+            <p>contato@kssolutions.tech</p>
+          </span>
+          <span>
+            <WhatsAppIcon />
+            <p>(71)99412-0997</p>
+          </span>
+          <span>
+            <LocationOnOutlinedIcon />
+            <p>
+              Edifício Macêdo - R. Francisco Drumond, 41 - Centro, Camaçari -
+              BA, 42800-063
+            </p>
+          </span>
+        </div>
+      </div>
+      <div className="contact-form-container">
+        <form onSubmit={handleSubmit} className="form">
+          <h3>Tem interesse em...</h3>
+          <div className="checkbox">
+            <span>
+              <input type="checkbox" />
+              <label className="checkbox-label">teste</label>
+            </span>
+          </div>
+          <div className="form-msg">
+            <label htmlFor="name">Seu nome:</label>
+            <input id="name" type="text" name="name" />
+            <ValidationError prefix="Name" field="name" errors={state.errors} />
+            <label htmlFor="email">Seu melhor e-mail:</label>
+            <input id="email" type="email" name="email" />
+            <ValidationError
+              prefix="Email"
+              field="email"
+              errors={state.errors}
+            />
+            <textarea
+              id="message"
+              name="message"
+              placeholder="Digite sua mensagem..."
+            />
+            <ValidationError
+              prefix="Message"
+              field="message"
+              errors={state.errors}
+            />
+            <button type="submit" disabled={state.submitting}>
+              Enviar menssagem
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
   );
 }
