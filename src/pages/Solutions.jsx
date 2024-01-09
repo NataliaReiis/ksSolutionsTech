13e9f82c67ce72a25abcc2ccb11308d582219c26
NataliaReiis <nataliareis208@gmail.com>
2024-01-08 23:25:57 -0300

    feat: under construct / solutions

--- src/pages/Solutions.jsx
+++ src/pages/Solutions.jsx
@@ -1,17 +1,17 @@
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
+import construct from "../assets/svg/construct.svg";
 
 export default function Solutions() {
-
-
-    
-    return(
+  return (
     <>
-        <Navbar />
-        
-        
-        <Footer />
-       
+      <Navbar />
+      <div style={{textAlign:'center', margin:'5rem 0'}} className="construction-container">
+        <h1 style={{color: '#0C1633', fontSize:'2em', margin:'3rem 0'}}>Essa Página está em construção</h1>
+        <img style={{width: '50%'}} src={construct} alt="" />
+      </div>
+
+      <Footer />
     </>
-    )
+  );
 }
