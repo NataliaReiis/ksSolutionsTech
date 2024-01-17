f502c34edb8b4d90796427774a62c901fe91228e
NataliaReiis <nataliareis208@gmail.com>
2024-01-17 14:50:34 -0300

    feat: adjsutment style responive mode --> solutions Ks

--- src/pages/Solutions.jsx
+++ src/pages/Solutions.jsx
@@ -7,8 +7,8 @@ export default function Solutions() {
     <div  className="solutions-container">
       <Navbar />
       <div style={{textAlign:'center', margin:'5rem 0'}} className="construction-container">
-        <h1 style={{color: '#0C1633', fontSize:'2em', margin:'3rem 0'}}>Essa Página está em construção</h1>
-        <img style={{width: '50%'}} src={construct} alt="" />
+        <h1 style={{color: '#0C1633', fontSize:'1.5em', margin:'3rem 0'}}>Essa Página está em construção</h1>
+        <img style={{width: '100%'}} src={construct} alt="" />
       </div>
 
       <Footer />
